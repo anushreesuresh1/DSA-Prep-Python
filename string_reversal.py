d464ee433ed05c6eace5a68f447c97fd0e4c0173
anushreesuresh10 <147473022+anushreesuresh10@users.noreply.github.com>
2025-05-27 03:05:32 +0530

    Update string_reversal.py

--- string_reversal.py
+++ string_reversal.py
@@ -12,9 +12,21 @@ def reverse_string_loop(input_string):
     return reversed_string
 
 
+
+def reverse_string_in_place(input_string):
+    start, end = 0, len(input_string) - 1
+    while start < end:
+        input_string[start], input_string[end] = input_string[end], input_string[start]
+        start += 1
+        end -= 1
+    return ''.join(input_string)
+
+
+
 if __name__ == "__main__":
     input_string = "Hello, World!"
     print("Original String:", input_string)
     print("Reversed String:", reverse_string(input_string))
     print("Reversed String (using loop):", reverse_string_loop(input_string))
+    print("Reversed String (in place):", reverse_string_in_place(list(input_string)))
 
